d44be83b839669004b78966c1c58e581d90b8d52
AoschkA <jonaspraem@gmail.com>
2016-10-30 18:46:31 +0100

    Added basic controls 2

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+    public float movementSpeed = 10.0F;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * movementSpeed;
+        float straffe = Input.GetAxis("Horizontal") * movementSpeed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+
+	}
+}
