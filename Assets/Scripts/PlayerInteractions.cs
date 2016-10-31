ff4bf99a9af5837c563747f440c239b0e66845a8
Jonas Praem <Jonas Praem>
2016-10-31 14:57:35 +0100

    added interactionScripts

---
+++ Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteractions : MonoBehaviour {
+
+    public float interactionDistance = 5.0f;
+
+    void Update () {
+
+        // Checks if any NPC is interactable 
+        if (Physics.Raycast(this.transform.position, -Vector3.up, interactionDistance)) {
+            print("interaction");
+        } else {
+            print("no interaction");
+        }
+	}
+}
